e492bedc9ad7b5ff15d6399ab3a69a0d1059f8c1
emmaru <littleoduozu@gmail.com>
2024-05-15 23:54:54 +0900

    versioningの導入

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\V1\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+Route::apiResource('tickets', TicketController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
