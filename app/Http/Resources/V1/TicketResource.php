a386a377174ece9f9be952adaccdbc50d93ec89c
emmaru <littleoduozu@gmail.com>
2024-05-19 18:31:51 +0900

    レスポンスの設計

---
+++ app/Http/Resources/V1/TicketResource.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use App\Models\Ticket;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin Ticket
+ */
+class TicketResource extends JsonResource
+{
+    // public static $wrap = 'ticket';
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'type' => 'ticket',
+            'id' => $this->id,
+            'attributes' => [
+                'title' => $this->title,
+                'description' => $this->description,
+                'status' => $this->status,
+                'createdAt' => $this->created_at,
+                'updatedAt' => $this->updated_at,
+            ],
+            'relationships' => [
+                'author' => [
+                    'data' => [
+                        'type' => 'user',
+                        'id' => $this->user_id
+                    ],
+                    'links' => [
+                        ['self' => 'todo']
+                    ]
+                ]
+            ],
+            'links' => [
+                ['self' => route('tickets.show', ['ticket' => $this->id])],
+            ],
+        ];
+    }
+}
